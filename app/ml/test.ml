3aa393b0b44585575c9f53b5a567d3bd0ecb4535
Danny Willems <contact@danny-willems.be>
2016-05-25 13:03:43 +0200

    Add opam pkg and change init config in Makefile, update with opam pin

--- app/ml/test.ml
+++ app/ml/test.ml
@@ -1,12 +1,13 @@
 let on_device_ready _ =
-  let status = Statusbar.t () in
+  let t = Cordova_statusbar.t () in
+  t#background_color_by_name "red";
   (*status#style_default;*)
   (*status#style_light_content;*)
   (*status#style_black_translucent;*)
   (*status#style_black_opaque;*)
   (*status#hide;*)
   (*status#show;*)
-  status#background_color_by_name "red";
+  (*status#background_color_by_name "red";*)
   Js._false
 
 let _ =
